5551b9259ccf3114947bbf1262ded588c7792092
Jakmobs <jakmobs@gmail.com>
2024-12-10 22:13:50 +0800

    Add files via upload

---
+++ websitejournal.js
@@ -0,0 +1,15 @@
+function church(){
+    window.location.href = "church.html"
+}
+
+function homeButton(){
+    window.location.href = "index.html"
+}
+
+function classroomButton(){
+    window.location.href = "classroom.html"
+}
+
+function courtButton(){
+    window.location.href = "court.html"
+}
